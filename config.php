3cefcf9352f908bb9c3f9215e3aaa0ed88052324
Rajbir Kr Thakur <rajbirkrthakur@gmail.com>
2026-07-28 20:32:47 +0530

    Update database configuration parameters

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 // Database configuration
 $db_host = "127.0.0.1";
-$db_port = 3306;
+$db_port = 3307;
 $db_user = "root";
 $db_pass = "";
-$db_name = "campus_booking";
+$db_name = "key_managment";
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name, $db_port);
 
